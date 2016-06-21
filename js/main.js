4c3d3a87931131cdedce1f88b436ddeb367cd894
Ahmed Soliman <ahmedanwar@mail.com>
2016-06-21 02:05:41 -0700

    finishing with an if inside the existing if

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ while(slaying) {
     if(youHit = 1) {
         console.log("You hit the dragon")
         totalDamage += damageThisRound;
+        //if happend for times the game ends
+        if(totalDamage >= 4 ) {
+                console.log("You slew the dragon");
+                slaying = false;
+                //if not it repeats
+                } else {
+                    youHit = Math.floor(Math.random() * 2);
+                    }
             //else
             }else {
         console.log("The dragon defeated you!")
