51c72e512e8d18132af994c1ebe04b06c9e34e09
Ahmed Soliman <ahmedanwar@mail.com>
2016-06-21 02:03:49 -0700

    added the first while and if/elss

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ var slaying = true;
 var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random() *5 + 1);
 var totalDamage = 0;
+
+//while loop
+while(slaying) {
+    // if loop
+    if(youHit = 1) {
+        console.log("You hit the dragon")
+        totalDamage += damageThisRound;
+            //else
+            }else {
+        console.log("The dragon defeated you!")
+    }
+}
